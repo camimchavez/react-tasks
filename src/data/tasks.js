7a32e4e84d76b9ecedf5539fdaa6d5446a65c623
Camila Chavez <82542492+camimchavez@users.noreply.github.com>
2022-10-25 10:46:10 -0300

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,22 @@
+export const tasks = [
+    {
+        id: 0,
+        title : "mi primer tarea",
+        description: "mi primer tarea"
+    },
+    {
+        id: 1,
+        title : "mi segunda tarea",
+        description: "mi segunda tarea"
+    },
+    {
+        id: 2,
+        title : "mi tercer tarea",
+        description: "mi tercer tarea"
+    },
+    {
+        id: 3,
+        title : "mi cuarta tarea",
+        description: "mi cuarta tarea"
+    },
+]
